ee4d15095708d12a61408e4eabce9b1fffe8f4e2
Luc <lukas1.schoettner@st.oth-regensburg.de>
2024-01-13 20:04:55 +0100

    cleanup in preparation of merge

--- src/main/java/de/othr/fitnessapp/repository/CourseRepository.java
+++
@@ -1,7 +0,0 @@
-package de.othr.fitnessapp.repository;
-
-import de.othr.fitnessapp.model.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepository extends JpaRepository<Course, Long> {
-}
--- src/main/java/de/othr/fitnessapp/repository/WorkoutRepository.java
+++
@@ -1,7 +0,0 @@
-package de.othr.fitnessapp.repository;
-
-import de.othr.fitnessapp.model.Workout;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WorkoutRepository extends JpaRepository<Workout, Long> {
-}
